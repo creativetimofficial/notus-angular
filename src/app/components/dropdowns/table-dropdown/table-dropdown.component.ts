2cd82c016ef6b1cabc3ee537e756f64ec96deb5c
EINazare <emanuelioannazare@gmail.com>
2020-09-04 17:31:03 +0300

    feature: add table dropdown

---
+++ src/app/components/dropdowns/table-dropdown/table-dropdown.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+import { createPopper } from "@popperjs/core";
+
+@Component({
+  selector: "app-table-dropdown",
+  templateUrl: "./table-dropdown.component.html",
+})
+export class NotificationDropdownComponent implements OnInit {
+  dropdownPopoverShow = false;
+  @ViewChild("btnDropdownRef", { static: false }) btnDropdownRef: ElementRef;
+  popper = document.createElement("div");
+  ngOnInit() {
+    this.popper.innerHTML = `<div class="bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg min-w-48" #popoverDropdownRef>
+  <a href="#pablo" class="text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent  text-gray-800">
+    Action
+  </a>
+  <a href="#pablo" class="text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent  text-gray-800">
+    Another action
+  </a>
+  <a href="#pablo" class="text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent  text-gray-800">
+    Something else here
+  </a>
+</div>`;
+  }
+  toggleDropdown(event) {
+    event.preventDefault();
+    if (this.dropdownPopoverShow) {
+      this.dropdownPopoverShow = false;
+      this.destroyPopper();
+    } else {
+      this.dropdownPopoverShow = true;
+      this.createPoppper();
+    }
+  }
+  destroyPopper() {
+    this.popper.parentNode.removeChild(this.popper);
+  }
+  createPoppper() {
+    createPopper(this.btnDropdownRef.nativeElement, this.popper, {
+      placement: "bottom-end",
+    });
+    this.btnDropdownRef.nativeElement.parentNode.insertBefore(
+      this.popper,
+      this.btnDropdownRef.nativeElement.nextSibling
+    );
+  }
+}
