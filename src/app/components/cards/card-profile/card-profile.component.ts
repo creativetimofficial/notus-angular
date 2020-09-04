4bbf8819784ab8e4828f8f9c8a51122fe455c820
EINazare <emanuelioannazare@gmail.com>
2020-09-04 18:00:45 +0300

    feature: add card-profile

---
+++ src/app/components/cards/card-profile/card-profile.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-profile',
+  templateUrl: './card-profile.component.html'
+})
+export class CardProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
