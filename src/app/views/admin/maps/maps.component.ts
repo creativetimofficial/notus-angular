52df4f818fdfb8fdaecc7bd9228b6edbfa01e880
EINazare <emanuelioannazare@gmail.com>
2020-09-04 19:01:43 +0300

    feature: add map view

---
+++ src/app/views/admin/maps/maps.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-maps',
+  templateUrl: './maps.component.html',
+  styleUrls: ['./maps.component.css']
+})
+export class MapsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
