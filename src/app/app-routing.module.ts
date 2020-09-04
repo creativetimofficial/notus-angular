df05ed1174dc963dfab0412c91e22bad9443db52
EINazare <emanuelioannazare@gmail.com>
2020-09-04 19:28:35 +0300

    refactor: add all routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,51 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
-import { DashboardComponent } from "./views/dashboard/dashboard.component";
+// layouts
+import { AdminComponent } from './layouts/admin/admin.component';
+import { AuthComponent } from './layouts/auth/auth.component';
+
+// admin views
+import { DashboardComponent } from "./views/admin/dashboard/dashboard.component";
+import { MapsComponent } from './views/admin/maps/maps.component';
+import { SettingsComponent } from './views/admin/settings/settings.component';
+import { TablesComponent } from './views/admin/tables/tables.component';
+
+// auth views
+import { LoginComponent } from './views/auth/login/login.component';
+import { RegisterComponent } from './views/auth/register/register.component';
+
+// no layouts views
+import { IndexComponent } from './views/index/index.component';
+import { LandingComponent } from './views/landing/landing.component';
+import { ProfileComponent } from './views/profile/profile.component';
 
 const routes: Routes = [
-  { path: "", redirectTo: "dashboard", pathMatch: "full" },
-  { path: "dashboard", component: DashboardComponent },
-];
+  // admin views
+  {
+    path: "/admin", component: AdminComponent, children: [
+      { path: "/admin/dashboard", component: DashboardComponent },
+      { path: "/admin/settings", component: SettingsComponent },
+      { path: "/admin/tables", component: TablesComponent },
+      { path: "/admin/maps", component: MapsComponent },
+    ]},
+    // auth views
+    {
+      path: "/auth", component: AuthComponent, children: [
+        { path: "/auth/login", component: LoginComponent },
+        { path: "/auth/register", component: RegisterComponent },
+      ]},
+      // no layout views
+      { path: "profile", component: ProfileComponent },
+      { path: "landing", component: LandingComponent },
+      { path: "/", component: IndexComponent },
+      { path: "/admin", redirectTo: "/admin/dashboard", pathMatch: "full" },
+      { path: "/auth", redirectTo: "/auth/login", pathMatch: "full" },
+      { path: "**", redirectTo: "/", pathMatch: "full" },
+    ];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-})
-export class AppRoutingModule {}
+    @NgModule({
+      imports: [RouterModule.forRoot(routes)],
+      exports: [RouterModule],
+    })
+    export class AppRoutingModule {}
