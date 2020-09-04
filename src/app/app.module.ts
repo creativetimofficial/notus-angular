e85357a5d06a0baa99c7c9695de9c3ddfdee0c1d
EINazare <emanuelioannazare@gmail.com>
2020-09-04 19:21:15 +0300

    docs: re-arrange the imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,36 +3,47 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+
+// layouts
+import { AdminComponent } from './layouts/admin/admin.component';
+import { AuthComponent } from './layouts/auth/auth.component';
+
+// admin views
 import { DashboardComponent } from "./views/admin/dashboard/dashboard.component";
+import { MapsComponent } from './views/admin/maps/maps.component';
+import { SettingsComponent } from './views/admin/settings/settings.component';
+import { TablesComponent } from './views/admin/tables/tables.component';
+
+// auth views
+import { LoginComponent } from './views/auth/login/login.component';
+import { RegisterComponent } from './views/auth/register/register.component';
+
+// no layouts views
+import { IndexComponent } from './views/index/index.component';
+import { LandingComponent } from './views/landing/landing.component';
+import { ProfileComponent } from './views/profile/profile.component';
+
+// components for views and layouts
+
+import { AdminNavbarComponent } from './components/navbars/admin-navbar/admin-navbar.component';
+import { AuthNavbarComponent } from './components/navbars/auth-navbar/auth-navbar.component';
 import { CardBarChartComponent } from "./components/cards/card-bar-chart/card-bar-chart.component";
 import { CardLineChartComponent } from "./components/cards/card-line-chart/card-line-chart.component";
-import { NotificationDropdownComponent } from "./components/dropdowns/notification-dropdown/notification-dropdown.component";
-import { UserDropdownComponent } from "./components/dropdowns/user-dropdown/user-dropdown.component";
-import { SidebarComponent } from "./components/sidebar/sidebar.component";
-import { FooterComponent } from './components/footers/footer/footer.component';
-import { FooterSmallComponent } from './components/footers/footer-small/footer-small.component';
-import { FooterAdminComponent } from './components/footers/footer-admin/footer-admin.component';
 import { CardPageVisitsComponent } from './components/cards/card-page-visits/card-page-visits.component';
 import { CardProfileComponent } from './components/cards/card-profile/card-profile.component';
 import { CardSettingsComponent } from './components/cards/card-settings/card-settings.component';
 import { CardSocialTrafficComponent } from './components/cards/card-social-traffic/card-social-traffic.component';
 import { CardStatsComponent } from './components/cards/card-stats/card-stats.component';
 import { CardTableComponent } from './components/cards/card-table/card-table.component';
+import { FooterAdminComponent } from './components/footers/footer-admin/footer-admin.component';
+import { FooterComponent } from './components/footers/footer/footer.component';
+import { FooterSmallComponent } from './components/footers/footer-small/footer-small.component';
 import { HeaderStatsComponent } from './components/headers/header-stats/header-stats.component';
-import { MapExampleComponent } from './components/maps/map-example/map-example.component';
-import { AuthNavbarComponent } from './components/navbars/auth-navbar/auth-navbar.component';
-import { AdminNavbarComponent } from './components/navbars/admin-navbar/admin-navbar.component';
 import { IndexNavbarComponent } from './components/navbars/index-navbar/index-navbar.component';
-import { AdminComponent } from './layouts/admin/admin.component';
-import { AuthComponent } from './layouts/auth/auth.component';
-import { MapsComponent } from './views/admin/maps/maps.component';
-import { SettingsComponent } from './views/admin/settings/settings.component';
-import { TablesComponent } from './views/admin/tables/tables.component';
-import { LoginComponent } from './views/auth/login/login.component';
-import { RegisterComponent } from './views/auth/register/register.component';
-import { IndexComponent } from './views/index/index.component';
-import { LandingComponent } from './views/landing/landing.component';
-import { ProfileComponent } from './views/profile/profile.component';
+import { MapExampleComponent } from './components/maps/map-example/map-example.component';
+import { NotificationDropdownComponent } from "./components/dropdowns/notification-dropdown/notification-dropdown.component";
+import { SidebarComponent } from "./components/sidebar/sidebar.component";
+import { UserDropdownComponent } from "./components/dropdowns/user-dropdown/user-dropdown.component";
 
 @NgModule({
   declarations: [
