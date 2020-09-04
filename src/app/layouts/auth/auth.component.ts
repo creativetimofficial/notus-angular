99b2286fad25ae0589bb7f871ed3a5658cce48a9
EINazare <emanuelioannazare@gmail.com>
2020-09-04 18:56:17 +0300

    feature: add auth layout

---
+++ src/app/layouts/auth/auth.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html'
+})
+export class AuthComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
