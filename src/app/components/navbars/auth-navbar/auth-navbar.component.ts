7ab1f6fc5f5cff4a7ca6ed2a4361f7ac2a750775
EINazare <emanuelioannazare@gmail.com>
2020-09-04 18:42:24 +0300

    feature: add auth-navbar

---
+++ src/app/components/navbars/auth-navbar/auth-navbar.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-navbar',
+  templateUrl: './auth-navbar.component.html'
+})
+export class AuthNavbarComponent implements OnInit {
+
+  navbarOpen = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+  setNavbarOpen(){
+    this.navbarOpen = !this.navbarOpen;
+  }
+
+}
