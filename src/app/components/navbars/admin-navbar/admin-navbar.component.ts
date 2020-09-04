512285117018fa68742714a58452a2d15f30173a
EINazare <emanuelioannazare@gmail.com>
2020-09-04 18:36:16 +0300

    refactor: rename navbar to navbars/admin-navbar

---
+++ src/app/components/navbars/admin-navbar/admin-navbar.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-navbar',
+  templateUrl: './admin-navbar.component.html'
+})
+export class AdminNavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
