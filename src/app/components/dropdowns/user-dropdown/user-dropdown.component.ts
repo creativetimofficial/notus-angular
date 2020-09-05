1b1739f263f028c65f3f0fc94eb87ca6e898c690
EINazare <emanuelioannazare@gmail.com>
2020-09-05 19:12:29 +0300

    refactor: move popper body inside the template

--- src/app/components/dropdowns/user-dropdown/user-dropdown.component.ts
+++ src/app/components/dropdowns/user-dropdown/user-dropdown.component.ts
@@ -8,44 +8,25 @@ import { createPopper } from "@popperjs/core";
 export class UserDropdownComponent implements OnInit {
   dropdownPopoverShow = false;
   @ViewChild("btnDropdownRef", { static: false }) btnDropdownRef: ElementRef;
-  popper = document.createElement("div");
-  ngOnInit() {
-    this.popper.innerHTML = `<div class="bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg min-w-48" #popoverDropdownRef>
-  <a href="#pablo" class="text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent  text-gray-800">
-    Action
-  </a>
-  <a href="#pablo" class="text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent  text-gray-800">
-    Another action
-  </a>
-  <a href="#pablo" class="text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent  text-gray-800">
-    Something else here
-  </a>
-  <div class="h-0 my-2 border border-solid border-gray-200"></div>
-  <a href="#pablo" class="text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent  text-gray-800">
-    Seprated link
-  </a>
-</div>`;
-  }
+  @ViewChild("popoverDropdownRef", { static: false })
+  popoverDropdownRef: ElementRef;
+  ngOnInit() {}
   toggleDropdown(event) {
     event.preventDefault();
     if (this.dropdownPopoverShow) {
       this.dropdownPopoverShow = false;
-      this.destroyPopper();
     } else {
       this.dropdownPopoverShow = true;
       this.createPoppper();
     }
   }
-  destroyPopper() {
-    this.popper.parentNode.removeChild(this.popper);
-  }
   createPoppper() {
-    createPopper(this.btnDropdownRef.nativeElement, this.popper, {
-      placement: "bottom-end",
-    });
-    this.btnDropdownRef.nativeElement.parentNode.insertBefore(
-      this.popper,
-      this.btnDropdownRef.nativeElement.nextSibling
+    createPopper(
+      this.btnDropdownRef.nativeElement,
+      this.popoverDropdownRef.nativeElement,
+      {
+        placement: "bottom-start",
+      }
     );
   }
 }
