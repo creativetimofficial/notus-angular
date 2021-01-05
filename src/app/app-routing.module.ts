45ff1ca1950ec996e6352082cceaee7d319ace92
Risav Karna <git@risav.dev>
2021-01-05 06:30:07 +0545

    (chore) update to ngel

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -51,7 +51,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
