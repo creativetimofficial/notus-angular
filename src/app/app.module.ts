27e57bec4203d281d166a88771de08fbf1fcf0c9
Daniel Morfa <daniel.morfa@uvesolutions.com>
2024-01-07 01:55:13 +0100

    feat(core): migrate angular
    migrate to angular 17 and update the solutions of charts to ng2-charts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -47,6 +47,7 @@ import { PagesDropdownComponent } from "./components/dropdowns/pages-dropdown/pa
 import { NotificationDropdownComponent } from "./components/dropdowns/notification-dropdown/notification-dropdown.component";
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
 import { UserDropdownComponent } from "./components/dropdowns/user-dropdown/user-dropdown.component";
+import { NgChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
@@ -85,7 +86,7 @@ import { UserDropdownComponent } from "./components/dropdowns/user-dropdown/user
     LandingComponent,
     ProfileComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, NgChartsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
